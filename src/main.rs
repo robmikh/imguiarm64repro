f437a7641d978c8539a5401460c5f11de5e3b236
robmikh <rob.mikh@outlook.com>
2024-09-14 20:39:32 -0700

    revise sys repro

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ use imgui_sys::{
 
 fn main() {
     // Dummy ImGuiContext
-    let mut bytes = vec![0u8; std::mem::size_of::<ImGuiContext>()];
+    let mut bytes = vec![0u8; 1025];
     let context = bytes.as_mut_ptr() as *mut ImGuiContext;
     unsafe { igSetCurrentContext(context) };
 
-    // This will AV because our dummy context isn't set up properly, but you 
-    // can see that pos, cond, and pivot have incorrect values on the C++ side.
-    let pos = ImVec2::zero();
+    // When you step through igSetNextWindowPos and follow it to the C++ side, you can
+    // see that pos, cond, and pivot have incorrect values. It may or not trigger the
+    // assert in ImGui::SetNextWindowPos depending on what cond becomes. On my machine,
+    // cond becomes 1025 (seemingly from vec size above) and triggers the assert.
+    let pos = ImVec2 { x: 200.0, y: 300.0 };
     let cond = ImGuiCond_FirstUseEver; // 4
     let pivot = ImVec2::zero();
     unsafe { igSetNextWindowPos(pos, cond as i32, pivot) };
